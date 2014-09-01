437ea8c6ecb618b0fcd9c918dbb5031d7e383a61
James Garner <jamesjohngarner@gmail.com>
2014-09-01 14:43:53 +0000

    Added shopitems view into urls

--- game/apps/characters/urls.py
+++ game/apps/characters/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import patterns, url
-from .views import CharacterDetail, CharacterCreate
+from .views import CharacterDetail, CharacterCreate, ShopItems
 
 urlpatterns = patterns('',
     url(r'^(?P<pk>\d+)$', CharacterDetail.as_view(), name='detail'),
     url(r'^create/$', CharacterCreate.as_view(), name='create'),
+    url(r'^shop/$', ShopItems.as_view(), name='shop'),
 )
