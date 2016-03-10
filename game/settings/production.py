921ed66de87afa7977876ba13868d2df296cd851
James Garner <jamesjohngarner@gmail.com>
2016-03-10 14:59:48 +0000

    Removed secret key and created new key in offline local.py file

--- game/settings/production.py
+++ game/settings/production.py
@@ -16,8 +16,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '=-tq%u)e&swydyc45gi#$$(&!-o8l-(j4p#7oezyng4covj&73'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
