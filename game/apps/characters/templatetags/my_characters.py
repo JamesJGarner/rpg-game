d8797b2b1a204089f02f33642c2ad6511dd42049
James Garner <jamesjohngarner@gmail.com>
2016-03-11 08:45:03 +0000

    Fixed context error

--- game/apps/characters/templatetags/my_characters.py
+++ game/apps/characters/templatetags/my_characters.py
@@ -5,9 +5,7 @@ register = template.Library()
 
 @register.inclusion_tag("site/my_characters.html", takes_context=True)
 def my_characters(context):
-    request = context['request']
-    my_characters = Character.objects.filter(user=request.user).order_by('-name')
+    my_characters = Character.objects.filter(user=context.request.user).order_by('-name')
     return {
         "my_characters": my_characters,
-        "request": request
     }
