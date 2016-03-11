d272cee0115a4583634a10f0f50ef525627efa7c
James Garner <jamesjohngarner@gmail.com>
2016-03-11 09:01:39 +0000

    Re-added login redirect url

--- game/urls.py
+++ game/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls import patterns, include, url
+from django.conf.urls import include, url
 from .views import Homepage
 from game.apps.characters.views import CharacterLeaderboard
 from django.contrib import admin
