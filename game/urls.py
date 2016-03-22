fdd07b92e5973df9fdca25e3e232dc0a225e0461
James Garner <jamesjohngarner@gmail.com>
2016-03-22 16:49:28 +0000

    Added media url and updated character item loop in template

--- game/urls.py
+++ game/urls.py
@@ -3,6 +3,8 @@ from .views import Homepage
 from game.apps.characters.views import CharacterLeaderboard
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
+from django.conf import settings
+from django.conf.urls.static import static
 admin.autodiscover()
 
 urlpatterns = [
@@ -14,4 +16,4 @@ urlpatterns = [
     url(r'^login/$', auth_views.login),
     url(r'^logout/$', auth_views.logout),
     url(r'^leaderboard/$', CharacterLeaderboard.as_view(), name="leaderbaord"),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
