f34c54fd2fe2ab523c0b2a9b1d75c545ad682a54
James Garner <jamesjohngarner@gmail.com>
2016-03-25 00:37:20 +0000

    Removed changing urls as just didn't seem natural

--- game/apps/userprofile/urls.py
+++ game/apps/userprofile/urls.py
@@ -2,5 +2,5 @@ from django.conf.urls import patterns, url
 from .views import UserProfilePage
 
 urlpatterns = [
-    url(r'^settings/$', UserProfilePage.as_view(), name="profile_page"),
+    url(r'^user/(?P<pk>\d+)/$', UserProfilePage.as_view(), name="profile_page"),
 ]
--- game/apps/userprofile/views.py
+++ game/apps/userprofile/views.py
@@ -3,6 +3,7 @@ from django.views.generic.edit import ModelFormMixin
 from .forms import UserProfileForm
 from .models import UserProfile
 from django.contrib.auth.models import User
+from game.apps.characters.models import Character
 
 class UserProfilePage(UpdateView):
     model = UserProfile
@@ -10,11 +11,11 @@ class UserProfilePage(UpdateView):
     form_class = UserProfileForm
     template_name = 'userprofile/UserProfile_form.html'
 
-   
-    def get_object(self):
-    	return User.objects.get(id=self.request.user.id)
-
-
     def form_valid(self, form):
         self.object = form.save()
         return super(ModelFormMixin, self).form_valid(form)
+
+    def get_context_data(self, **kwargs):
+        context = super(UserProfilePage, self).get_context_data(**kwargs)
+        context['characters'] = Character.objects.filter(user=self.object.pk)
+        return context
