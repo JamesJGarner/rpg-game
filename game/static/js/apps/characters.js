e665669a8d9371843b9ee4da46c41bcfe11024b2
James Garner <jamesjohngarner@gmail.com>
2016-03-29 16:41:20 +0100

    Updated characters.js with naming conventions that make more sense added and rewrote functions

--- game/static/js/apps/characters.js
+++ game/static/js/apps/characters.js
@@ -1,104 +1,162 @@
-var selected = null;
+var SpellAcquired_id = null;
+var ItemAcquired_id = null;
+var ItemAcquired = null;
 
-$(".cd-spells li").on("click", function(e){
-	selected = $('#' + this.id + '-dropdown ul').html();
-	$('#sidebar').html(selected)
-	e.preventDefault();
-});
+
+/* Hover Events */
+
+$(".cd-character-items li").hover(
+    function() {
+        if (!$('#' + this.id).hasClass("cd-selected")) { //Can this be improved in the future?
+            HoverOn("item", "inv-" + $('#' + this.id + ' img')[0].id);
+        }
+    },
+    function() {
+      HoverOff("item");
+  }
+)
+
+
+$(".cd-inventory-bag li").hover(
+  function() {
+    HoverOn("item", this.id)
+  },
+  function() {
+    HoverOff("item");
+  }
+);  
 
 
 $(".cd-spells li").hover(
   function() {
-      $('#sidebar').html($('#' + id + '-dropdown ul').html())
-  }, function() {
-  	if (selected != null) {
-  		$('#sidebar').html(selected)
-  	}
-  	else {
-    	$('#sidebar').empty();
-	}
+    HoverOn("spell", this.id)
+  },
+  function() {
+    HoverOff("spell");
   }
 );
 
-var inv_selected = null;
-var item_selected = null;
-var item_id = null;
-var item_image = null;
-var equipped_to = null;
+
+function HoverOn(type, id) {
+  $(elements(type)).html($('#' + id + '-dropdown ul').html())
+}
+
+
+function HoverOff(type, id) {
+  var html = null;
+  var element = elements(type);
+
+  if (type == "item") {
+    html = $('#inv-' + ItemAcquired_id + '-dropdown ul').html();
+  }
+  else if (type == "spell") {
+    html = selected;
+  }
+
+  if (html != null) {
+    $(element).html(html);
+  }
+  else {
+    $(element).empty();
+  }
+}
+
+
+function elements(type) {
+  if (type == "item") {
+    return "#inv-sidebar";
+  }
+  else if (type == "spell") {
+    return "#sidebar";
+  }
+  else {
+    throw "No type defined as " + type; 
+  }  
+}
+
+/* Click Events */
+
+$('.cd-character-items li').on("click", UnequipItem);
+$(".cd-spells li").on("click", SelectSpell);
+
 
 $(".cd-inventory-bag li").on("click", function(e){
   $('.cd-character-items li').removeClass("cd-selected").off("click");
   $('.cd-inventory-bag li').removeClass("inv-sidebar-selected");
-	inv_selected = $('#' + this.id + '-dropdown ul').html();
-  item_selected = this.id.replace('inv-','');
-	$('#inv-sidebar').html(inv_selected)
   $('#' + this.id).addClass("inv-sidebar-selected");
-	e.preventDefault();
-  ItemLookup(item_selected);
+	$('#inv-sidebar').html($('#' + this.id + '-dropdown ul').html())
+
+  ItemAcquired_id = this.id.replace('inv-','');
+  ItemLookup(ItemAcquired_id);
 });
+
+
+
+function SelectSpell() {
+  SpellAcquired_id = this.id;
+  $('#sidebar').html($('#' + SpellAcquired_id + '-dropdown ul').html())
+}
+
+
+function resetVars() {
+  $('.cd-character-items li').removeClass("cd-selected").off("click").on("click", UnequipItem);
+  $('.cd-inventory-bag li').removeClass("inv-sidebar-selected");
+  ItemAcquired_id = null;
+  $('#inv-sidebar').empty();
+}
 
 
 function ItemLookup(id) {
   $.getJSON( "/api/item/" + id, function( data ) {
-    var items = [];
-    $.each(data, function( key, val ) {
-      if (key == "equipped_to") {
-        equipped_to = val;
-      }
-    });
-    $.each(data.item, function( key, val ) {
-      if (key == "image") {
-        item_image = val;
-      }
-      if (key == "id") {
-        item_id = val;
-      }
-      if (key == "position") {
-        for (i in val) {
-          $('#' + String(val[i]).replace(' ', '-').toLowerCase()).addClass("cd-selected").on('click', EquipItem);
-        }
-      }
-    });
+    ItemAcquired = data;
+    for (i in data.item.position) {
+      $('#' + String(data.item.position[i]).replace(' ', '-').toLowerCase()).addClass("cd-selected").on('click', EquipItem);
+    }
   });
 };
 
 
 function EquipItem() {
-  var that = this;
   var form = "character=1&equipped_to=" + this.id
-  $.ajax({
-    beforeSend: function(xhr, settings) {
-      xhr.setRequestHeader("X-CSRFToken",  getCookie('csrftoken'));
-    },
-    url: "/api/items/" + item_selected + "/",
-    type: "PATCH",
-    data: form,
-    success: function(data) {
-      $('#inv-' + item_selected).addClass("cd-item-hide");
-      $('#'+ equipped_to + ' img').attr('src', "");
-      $('#' + that.id + ' img').attr('src', item_image).attr('id', item_selected);
-      resetVars();
-    }
-  });
+  var url = ItemAcquired_id;
+  ItemForm(form, url, EquipItemSuccess(this.id, url))
 }
 
 
-function UnequipItem(current, itemid) {
+function UnequipItem() {
   var form = "character=1&equipped_to=";
+  var url = $('#' + this.id + ' img')[0].id;
+  ItemForm(form, url, UnequipItemSuccess(this.id, url))
+}
+
+
+function EquipItemSuccess(id, item) {
+  $('#inv-' + item).addClass("cd-item-hide");
+  $('#'+ ItemAcquired.equipped_to + ' img').attr('src', "");
+  $('#' + id + ' img').attr('src', ItemAcquired.item.image).attr('id', item);
+  resetVars(); 
+}
+
+
+function UnequipItemSuccess(id, item) {
+  $('#'+ id + ' img').attr('src', "").attr('id', "");
+  $('#inv-' + item).removeClass("cd-item-hide");
+  resetVars();
+}
+
+
+function ItemForm(form, url, successFunction) {
   $.ajax({
     beforeSend: function(xhr, settings) {
       xhr.setRequestHeader("X-CSRFToken",  getCookie('csrftoken'));
     },
-    url: "/api/items/" + itemid + "/",
+    url: "/api/items/" + url + "/",
     type: "PATCH",
     data: form,
     success: function(data) {
-      $('#'+ current + ' img').attr('src', "");
-      $('#inv-' + itemid).removeClass("cd-item-hide");
-      //$('#' + that.id + ' img').attr('src', item_image);
-      resetVars();
+      successFunction;
     }
-  });
+  }); 
 }
 
 
@@ -107,62 +165,6 @@ $( document ).on( 'keydown', function ( e ) {
         resetVars();
     }
 });
-
-
-function resetVars() {
-  $('.cd-character-items li').removeClass("cd-selected").off("click").on("click", dothething);
-  $('.cd-inventory-bag li').removeClass("inv-sidebar-selected");
-  item_selected = null;
-  $('#inv-sidebar').empty();
-  inv_selected = null;
-}
-
-
-$(".cd-character-items li").hover(
-    function() {
-        if (!$('#' + this.id).hasClass("cd-selected")) { //Can this be improved in the future?
-            hoverid("inv-" + $('#' + this.id + ' img')[0].id);
-        }
-    },
-    function() {
-      unhover()
-    }
-)
-
-
-$('.cd-character-items li').on("click", dothething);
-
-
-function dothething() {
-      if (!$('#' + this.ifd).hasClass("cd-selected")) { //Can this be improved in the future?
-        var itemid = $('#' + this.id + ' img')[0].id;
-        UnequipItem(this.id, itemid);
-    }
-}
-
-
-$(".cd-inventory-bag li").hover(
-  function() {
-    hoverid(this.id)
-  }, function() {
-      unhover();
-  }
-);  
-
-
-function unhover() {
-      if (inv_selected != null) {
-      $('#inv-sidebar').html(inv_selected);
-    }
-    else {
-      $('#inv-sidebar').empty();
-   }
-}
-
-
-function hoverid(id) {
-  $('#inv-sidebar').html($('#' + id + '-dropdown ul').html())
-}
 
 
 function getCookie(name) {
