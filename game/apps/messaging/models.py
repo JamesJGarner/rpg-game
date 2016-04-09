79fe34db0fbc897f213f13f52f758872c361cd2d
James Garner <jamesjohngarner@gmail.com>
2016-04-09 21:17:53 +0100

    Added message model to the messaging app

--- game/apps/messaging/models.py
+++ game/apps/messaging/models.py
@@ -1,5 +1,24 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Message(models.Model):
+
+    from_user = models.ForeignKey(
+        User,
+        related_name='from_user'
+        )
+
+    to_user = models.ForeignKey(
+        User
+        )
+
+    content = models.TextField()
+
+    date = models.DateTimeField()
+
+
+    def unicode(self):
+        return self.from_user
